43415e7903e59fb8cfcc01ac8d57b3920bf79f73
Ricardo Bernardelli <ricardobcs@gmail.com>
2011-10-10 15:45:45 -0300

    added first home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Sobre::Application.routes.draw do
+  get "welcome/index"
 
   devise_for :users
 
@@ -51,7 +52,7 @@ Sobre::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
