7a4cef97cd60bd5b52b50307fb9ba4001777505e
Ricardo Bernardelli <ricardobcs@gmail.com>
2011-10-11 17:12:35 -0300

    not needed username at this time

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :name
+  attr_accessible :email, :password, :remember_me, :username, :name
 
-  validates_presence_of :username
-  validates_uniqueness_of :username
+  validates_uniqueness_of :username, :allow_nil => true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,5 @@ require 'spec_helper'
 describe User do
   before(:each) { Factory(:user) }
 
-  it { should validate_presence_of :username }
   it { should validate_uniqueness_of :username }
 end
