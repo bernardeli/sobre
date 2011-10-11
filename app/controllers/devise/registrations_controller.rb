2803f077adfc644f84f9b6ac4c46219ed328f957
Ricardo Bernardelli <ricardobcs@gmail.com>
2011-10-11 16:56:17 -0300

    don't need it anymore

--- app/controllers/devise/registrations_controller.rb
+++
@@ -1,9 +0,0 @@
-class Devise::RegistrationsController < Devise::SessionsController
-  before_filter :set_password_confirmation, :only => [:create, :update]
-
-  private
-
-  def set_password_confirmation
-    params[:user][:password_confirmation] = params[:user][:password]
-  end
-end
