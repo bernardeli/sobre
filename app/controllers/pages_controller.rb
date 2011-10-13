628d93593e9de4028a33c90596b0980cc2e5b8c5
Ricardo Bernardelli <ricardobcs@gmail.com>
2011-10-13 15:44:16 -0300

    after updating a page, it must render nothing

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,12 @@ class PagesController < InheritedResources::Base
   before_filter :authenticate_user!
   actions :update
 
+  def update
+    update! do |format|
+      format.html { render :nothing => true  }
+    end
+  end
+
   private
 
   def resource
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,4 +1,13 @@
 require 'spec_helper'
 
 describe PagesController do
+  let(:user) { Factory(:user) }
+  before(:each) { sign_in user }
+
+  describe "PUT update" do
+    it "render nothing" do
+      put :update, :id => user.page.id
+      response.body.should be_blank
+    end
+  end
 end
