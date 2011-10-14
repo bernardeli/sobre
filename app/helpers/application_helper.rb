ccec932a51d4419f517d387afcaafd257a547eaa
Ricardo Bernardelli <ricardobcs@gmail.com>
2011-10-14 15:51:57 -0300

    set theme according to user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def selected_class(path)
     current_page?(path) ? "selected" : ""
   end
+
+  def theme_class
+    current_page?(dashboard_index_path) ? current_user.theme : @user.theme
+  end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -15,4 +15,23 @@ describe ApplicationHelper do
       end
     end
   end
+
+  describe "#theme_class" do
+    context "when path is dashboard" do
+      before(:each) { helper.stub(:current_page?).and_return true }
+      it "returns signed in user theme" do
+        user = Factory(:user)
+        sign_in user
+        helper.theme_class.should == 'theme-01'
+      end
+    end
+
+    context "when path is not dashboard" do
+      it "returns user theme" do
+        @user = Factory(:user)
+        @user.stub(:theme).and_return('theme-02')
+        helper.theme_class.should == 'theme-02'
+      end
+    end
+  end
 end
