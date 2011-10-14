068705bd2f46c8a03bbfee729696edbba6b447f6
Ricardo Bernardelli <ricardobcs@gmail.com>
2011-10-14 11:37:22 -0300

    added postfix as email engine

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,8 @@ Sobre::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  config.action_mailer.delivery_method = :postfix
+
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
 
