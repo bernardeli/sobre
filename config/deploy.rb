1dcf84c36f50ee441ffcf62d74a618282ac6c400
Ricardo Bernardeli <ricardobcs@gmail.com>
2012-04-06 23:34:10 -0300

    fuck this asset shit line

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,5 @@
 require 'bundler/capistrano'
+load 'deploy/assets'
 
 set :application, "sobre"
 set :user, "deploy"
@@ -27,42 +28,20 @@ role :db, domain, :primary => true
 
 # If you are using Passenger mod_rails uncomment this:
 namespace :deploy do
- task :start do ; end
- task :stop do ; end
- task :restart, :roles => :app, :except => { :no_release => true } do
-   run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
- end
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
 
- task :symlink_config do
-   run "ln -nfs #{shared_path}/config/mongoid.yml #{release_path}/config/mongoid.yml"
- end
+  task :symlink_config do
+    run "ln -nfs #{shared_path}/config/mongoid.yml #{release_path}/config/mongoid.yml"
+  end
+
+  task :precompile do
+    run "cd #{release_path}; RAILS_ENV=production rake assets:precompile"
+  end
 end
 
-#namespace :god do
-# def god_is_running
-#   !capture("#{god_command} status >/dev/null 2>/dev/null || echo 'not running'").start_with?('not running')
-# end
-#
-# def god_command
-#   "cd #{current_path}; god"
-# end
-#
-# desc "Stop god"
-# task :terminate_if_running do
-#   if god_is_running
-#     run "#{god_command} terminate"
-#   end
-# end
-#
-# desc "Start god"
-# task :start do
-#   config_file = "#{current_path}/config/god/resque.god"
-#   environment = { :RAILS_ENV => rails_env, :RAILS_ROOT => current_path }
-#   run "#{god_command} -c #{config_file}", :env => environment
-# end
-#end
-#
-#before "deploy:update", "god:terminate_if_running"
-#after "deploy:update", "god:start"
-
-before "deploy:assets:precompile", "deploy:symlink_config"
+after "deploy:update_code", "deploy:symlink_config"
+after "deploy:symlink_config", "deploy:precompile"
